22fa36be79f03ecfbeb5b5201fb876d8eaa59316
m.yousefi <mohsen.u3fi@hotmail.com>
2023-11-16 20:56:55 +0330

    debug minio connection

--- routers/database/minio_connection.py
+++ routers/database/minio_connection.py
@@ -1,27 +1,24 @@
 import minio.error
 from minio import Minio
 
-from routers.config import settings
-
 
 class MinIoConnection:
     minio_endpoint = "91.107.176.230:9000"
-    minio_access_key = "zzyAWLk6AvHd2GiK5CjC"
-    minio_secret_key = "R4fMyjORMFFQJzgEMPM9XzugyYlMCZ8pJX4KvpiI"
+    minio_access_key = "6DC3MPymiCHnKrMDcM5Y"
+    minio_secret_key = "9j2taXeTguvBN4owGM6OZzyIh56QpzYlcbooaZtU"
     minio_bucket_name = "docsgenerator"
 
     def __init__(self):
         self.minio_connection = Minio(endpoint=self.minio_endpoint,
-                                      access_key="6DC3MPymiCHnKrMDcM5Y",
-                                      secret_key="9j2taXeTguvBN4owGM6OZzyIh56QpzYlcbooaZtU",
+                                      access_key=self.minio_access_key,
+                                      secret_key=self.minio_secret_key,
                                       secure=False
                                       )
 
     def upload_file(self, file_name, file_path):
         try:
-            a = self.minio_connection.bucket_exists(settings.MINIO_BUCKET_NAME)
             response = self.minio_connection.fput_object(
-                settings.MINIO_BUCKET_NAME, object_name=file_name, file_path=file_path
+                self.minio_bucket_name, object_name=file_name, file_path=file_path
             )
             return response, True
         except Exception as err:
@@ -29,9 +26,9 @@ class MinIoConnection:
 
     def download_file(self, object_name):
         try:
-            response = self.minio_connection.get_object(settings.MINIO_BUCKET_NAME, object_name=object_name)
+            response = self.minio_connection.get_object(self.minio_bucket_name, object_name=object_name)
             return response
-        except minio.error as err:
+        except Exception as err:
             return f"File download failed: {err}"
 
 
